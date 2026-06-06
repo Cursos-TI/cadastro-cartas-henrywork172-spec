2b18f3aa3aa3aceb00b746791de8b61f61d7e9cb
henrywork172-spec <henrywork172@gmail.com>
2026-06-06 19:33:14 -0300

    Adicionei densidade populacional e pib percapita via double
    Aprendi a usar double (%lf) e adicionei dois dados de operadores matemáticos: Densidade Populacional e PIB per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main(){
+  char estado[50];
+  char codigo_carta[20], nome_cidade[20];
+  int populacao;
+  int pontos_turisticos;
+  float area_km2;
+  double PIB, densidade_populacional, PIB_percapita;
+  
+  printf("Insira o estado: \n");
+  scanf("%s", estado);
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  printf("Insira o código da carta: \n");
+  scanf("%s", codigo_carta);
 
-  // Área para entrada de dados
+  printf("Insira o nome da cidade: \n");
+  scanf("%s", nome_cidade);
 
-  // Área para exibição dos dados da cidade
+  printf("Insira a quantidade populacional: \n");
+  scanf("%d", &populacao);
 
-    printf("Hello, World!\n");
-    printf("Exemplo de edição de código!\n");
+  printf("Insira o Nº de pontos turísticos: \n");
+  scanf("%d", &pontos_turisticos);
 
-return 0;
-} 
+  printf("Insira a area em km2: \n");
+  scanf("%f", &area_km2);
+
+  printf("Insira o PIB: \n");
+  scanf("%lf", &PIB);
+
+  printf("Estado: %s - Código da Carta: %s - Nome da cidade: %s \n", estado, codigo_carta, nome_cidade);
+  printf("Populacao: %d - Pontos turisticos: %d - Area em Km2: %.2f - PIB: %.2f \n", populacao, pontos_turisticos, area_km2, PIB);
+
+  densidade_populacional = populacao / area_km2;
+  PIB_percapita = PIB / populacao;
+
+  printf("Densidade Populacional: %.2lf - PIB per capita: %.2lf \n", densidade_populacional, PIB_percapita);
+
+  return 0;
+
+}
